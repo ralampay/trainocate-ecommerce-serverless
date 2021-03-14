36896c665b7b3beee431675f2ddeb204ae9cf0bc
Raphael Alampay <raphael.alampay@gmail.com>
2021-03-15 00:55:58 +0800

    checkpoint

--- fetch_courses/app.py
+++ fetch_courses/app.py
@@ -1,10 +1,32 @@
 import json
+import boto3
+import os
+from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
-  return {
+
+  # Which dynamodb endpoint we will connect to
+  endpoint_url  = "http://localhost:8000"
+  table_name    = os.environ['TABLE_NAME_COURSES']
+
+  # DynamoDB setup
+  dynamodb  = boto3.resource('dynamodb', endpoint_url=endpoint_url)
+  table     = dynamodb.Table(table_name)
+
+  try:
+    print("Fetching data from table %s..." % (table_name))
+    response  = table.scan()
+
+    print("Response:")
+    print(response)
+
+    return {
       "statusCode": 200,
       "body": json.dumps({
-      "message": "hello world",
-      # "location": ip.text.replace("\n", "")
-    }),
-  }
+        "courses": response["Items"]
+      })
+    }
+     
+  except ClientError as e:
+    print(e.response['Error']['Message'])
+    print("Something went wrong!")
